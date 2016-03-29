7faa60f9edbef84083b8cc6840ea6978033691b1
Tate <borigthedwarf@gmail.com>
2016-03-29 16:30:28 +1300

    Sliders for track progress, toolbars, nice looking buttons

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,50 +4,118 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollBar;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
+import javafx.geometry.Pos;
+import javafx.scene.*;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 import javax.swing.*;
 
 public class Main extends Application {
 
+
+    //The total length of the displayed waveform
+    final double TRACK_LENGTH = 60;
+
     Image wave = new Image(Main.class.getResourceAsStream("images/waveform_post.png"));
     ImageView iv1 = new ImageView();
+    Slider progressSlider1 = new Slider();
+    Slider progressSlider2 = new Slider();
     final int WIDTH = 1024;
     final int HEIGHT = 768;
     final double ZOOM = 1.2;
 
+    double secondsPerPixel = TRACK_LENGTH / WIDTH;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
 
+        progressSlider1.setMax(TRACK_LENGTH);
+        progressSlider2.setMax(TRACK_LENGTH);
+
         Pane root = new StackPane();
+
+        ToolBar toolBar = new ToolBar(
+                new Button("File")
+        );
         VBox vbox = new VBox();
         root.getChildren().add(vbox);
+        vbox.getChildren().add(toolBar);
 
         //Create a scene
         Scene scene = new Scene(root, WIDTH, HEIGHT);
 
-        drawWaveformBox(vbox);
-        HBox buttonsBox = new HBox();
-        vbox.getChildren().add(buttonsBox);
+        StackPane waveFormStack = new StackPane();
+        drawWaveformBox(waveFormStack);
+
+        vbox.getChildren().add(waveFormStack);
+
+        waveFormStack.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                progressSlider1.setValue(mouseEvent.getSceneX()*secondsPerPixel);
+                //progressSlider2.setValue(mouseEvent.getSceneX()*secondsPerPixel);
+            }
+        });
+
+        waveFormStack.setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                progressSlider2.setValue(mouseEvent.getSceneX()*secondsPerPixel);
+            }
+        });
+
+        vbox.getChildren().add(progressSlider1);
+        vbox.getChildren().add(progressSlider2);
+
+        ToolBar zoomToolbar = new ToolBar(
+                plusButton(),
+                minusButton()
+        );
+        Separator sep = new Separator();
+        ToolBar playbackToolbar = new ToolBar(
+                new Button("|>"),
+                new Button("||"),
+                new Button("[]")
+        );
+        Separator sep2 = new Separator();
+        ToolBar splitAndJoinToolbar = new ToolBar(
+                new Button("Split"),
+                new Button("Join")
+        );
+        HBox.setHgrow(sep, Priority.ALWAYS);
+        HBox.setHgrow(sep2, Priority.ALWAYS);
+        HBox toolbars = new HBox();
+        toolbars.getChildren().addAll(zoomToolbar, sep, playbackToolbar, sep2, splitAndJoinToolbar);
+        toolbars.setPrefWidth(WIDTH);
+
+        vbox.getChildren().add(toolbars);
         vbox.getChildren().add(annotationBox());
-        drawZoomButtons(buttonsBox);
-        drawPlayerButtons(buttonsBox);
 
         primaryStage.setTitle("Transcription Assistance Toolkit");
         primaryStage.setScene(scene);
         primaryStage.sizeToScene();
         primaryStage.show();
+
+    }
+
+    public void zoomIn() {
+        iv1.setFitWidth(iv1.getFitWidth() * ZOOM);
+        secondsPerPixel = secondsPerPixel * 0.80;
+    }
+
+    public void zoomOut() {
+        if (iv1.getFitWidth() / ZOOM >= WIDTH) {
+            iv1.setFitWidth(iv1.getFitWidth() / ZOOM);
+            secondsPerPixel = secondsPerPixel / 0.80;
+        }
     }
 
 
@@ -59,7 +127,7 @@ public class Main extends Application {
         //Load image and resize
         iv1.setImage(wave);
         iv1.setFitHeight(200);
-        iv1.setFitWidth(root.getScene().getWidth());
+        iv1.setFitWidth(WIDTH);
 
         HBox box = new HBox();
         box.getChildren().add(iv1);
@@ -72,46 +140,31 @@ public class Main extends Application {
         root.getChildren().add(new BorderPane(sPane));
     }
 
-    private void drawZoomButtons(Pane root) {
-        //Create the buttons
-        Button zoomPlus = new Button("+");
+        public Button plusButton() {
+
+            Button zoom = new Button("+");
+
+            zoom.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    zoomIn();
+                }
+            });
+
+            return zoom;
+        }
+
+    public Button minusButton() {
         Button zoomMinus = new Button("-");
 
-        zoomPlus.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                iv1.setFitWidth(iv1.getFitWidth() * ZOOM);
-            }
-        });
         zoomMinus.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                if(iv1.getFitWidth() / ZOOM >= WIDTH )
-                    iv1.setFitWidth(iv1.getFitWidth() / ZOOM );
+                zoomOut();
             }
         });
 
-        HBox box = new HBox();
-
-        box.getChildren().add(zoomPlus);
-        box.getChildren().add(zoomMinus);
-
-        root.getChildren().add(box);
-
-    }
-
-    private void drawPlayerButtons(Pane root) {
-        Button play = new Button("   |>   ");
-        Button pause = new Button("   ||    ");
-        Button stop = new Button("   []    ");
-
-        HBox box = new HBox();
-
-        box.getChildren().add(play);
-        box.getChildren().add(pause);
-        box.getChildren().add(stop);
-
-        root.getChildren().add(box);
+        return zoomMinus;
     }
 
     private TextArea annotationBox() {
@@ -121,4 +174,6 @@ public class Main extends Application {
         text.setWrapText(true);
         return text;
     }
+
+    class Delta { double x, y; }
 }
