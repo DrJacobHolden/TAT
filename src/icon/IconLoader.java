e617df704023fae76ca9e0a68b7027771b735fd7
Tate <borigthedwarf@gmail.com>
2016-04-01 12:26:48 +1300

    Added new icons

--- src/icon/IconLoader.java
+++ src/icon/IconLoader.java
@@ -16,10 +16,18 @@ public class IconLoader {
         return instance;
     }
 
+    //Playback controls
     public Image prevIcon = new Image(getClass().getResourceAsStream("/sample/icons/previous-track.png"));
     public Image playIcon = new Image(getClass().getResourceAsStream("/sample/icons/play-button.png"));
     public Image nextIcon = new Image(getClass().getResourceAsStream("/sample/icons/next-track-button.png"));
     public Image pauseIcon = new Image(getClass().getResourceAsStream("/sample/icons/pause-button.png"));
     public Image stopIcon = new Image(getClass().getResourceAsStream("/sample/icons/stop.png"));
+
+    //Zoom controls
+    public Image zoomInIcon = new Image(getClass().getResourceAsStream("/sample/icons/zoom-in.png"));
+    public Image zoomOutIcon = new Image(getClass().getResourceAsStream("/sample/icons/zoom-out.png"));
+
+    //Menu Icons
+    public Image menuIcon = new Image(getClass().getResourceAsStream("/sample/icons/menu-1.png"));
 
 }
