fa65715b2bb2351aee3639ddbe52641e2132b9a6
Tate <borigthedwarf@gmail.com>
2016-07-15 13:32:24 +0800

    Revert "Imported audioFiles placed into corpus"
    This reverts commit cec92846c9f80f2cca3ae9ffa0ca5d021cb8afa2.

--- src/file_system/FileSystem.java
+++ src/file_system/FileSystem.java
@@ -156,6 +156,7 @@ public class FileSystem {
     }
 
     private void importRecordings() throws IOException {
+        System.out.println("called");
         recordings = Recording.groupSegments(loadSegments());
         checkForMissingSegments();
     }
@@ -208,16 +209,12 @@ public class FileSystem {
     public Recording importExternalRecording(File audioFile, File annotationFile) {
         Segment segment = new Segment(this);
         //Strip file extension
-        String baseName = audioFile.getPath().substring(audioFile.getPath().lastIndexOf(File.separator)+1, audioFile.getPath().lastIndexOf('.'));
-        System.out.println(baseName);
+        String baseName = audioFile.getPath().substring(0, audioFile.getPath().lastIndexOf('.'));
+
         if (recordings.get(baseName) != null) {
             //TODO: Throw error
         }
         segment.setBaseName(baseName);
-        //TODO: Move file to Corpus
-        System.out.println(pathFromString(segment, getAudioStorageRule()));
-        //audioFile.renameTo(pathFromString(segment, getAudioStorageRule()).toFile());
-
         segment.loadExternalAudioFile(audioFile);
         if (annotationFile != null) {
             segment.loadExternalAnnotationFile(annotationFile);
